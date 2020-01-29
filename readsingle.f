b99361b4e78d185c2e8c4aa9e6d6151d0687ca60
Pawel Sznajder <pawel.sznajder@ncbj.gov.pl>
2020-01-29 15:18:13 +0100

    wip: add function to read 3D files

---
+++ readsingle.f
@@ -0,0 +1,41 @@
+C     Read single file
+
+      function readinsingle(nx,nq,nt,path,isim) result(arr)
+
+      implicit none
+
+      integer nx,nq,nt
+      character(len=*) path
+      logical isim
+      real, dimension(nx,nq,nt) :: arr 
+
+      integer i,j,k
+      real*8 x,q,t
+      real*8 dum1,dum2
+
+      open(unit=11,file=path,status='unknown')
+
+      do j = 1, nx
+            read(11,*) x
+
+         do i = 1, nq
+            read(11,*) q
+            
+            do k = 1, nt
+                read(11,*) t,dum1,dum2
+
+                if(isim) then
+                    arr(j,i,k) = dum1
+                else
+                    arr(j,i,k) = dum2
+                end if
+
+            enddo
+         enddo
+      enddo
+
+      close(unit=11)
+
+      end function 
+
+
